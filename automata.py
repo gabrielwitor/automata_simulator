165023fb097250ac3f33abec2730e36af856ec9b
Gabriel Witor <witor.alm@gmail.com>
2024-05-22 18:40:49 -0300

    implementation using multimaps (defaultdict)

--- automata.py
+++ automata.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import csv
+from collections import defaultdict
 
 def main():
     # Opening the json file
@@ -14,22 +15,26 @@ def main():
 
     final_state = aut_dict['final']
 
-    # Creating a hash map for transitions. Format: 0a = 1
+    # Creating a multimap for transitions. Format: 0a = 1
 
-    transitions = {}
+    transitions = defaultdict(list)
 
     for transition in aut_dict['transitions']:
-        transitions[transition['from']+transition['read']] = transition['to']
+        transitions[transition['from']+transition['read']].append(transition['to'])
+
+    print(transitions)
+
 
     input_csv = open(sys.argv[2])
     csv_reader = csv.reader(input_csv, delimiter=';')
 
     for input in csv_reader:
         q = initial_state
+        # input[0] since we are processing the word (Ex: aaabaabbb) and not the expected result.
         for symbol in input[0]:
             key = f'{q}{symbol}'
             if (key in transitions):
-                q = transitions[key]
+                q = transitions[key][0]
             else:
                 q = -1
         print(int(q) in final_state)
